f02662889532df74aeca58c4f8314df34b9a932c
Christopher Stevers <christopher.stevers@protonmail.com>
2022-06-10 14:00:57 -0400

    prettier/eslint

--- src/schema.js
+++ src/schema.js
@@ -1,82 +1,82 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-    type PullRequest {
-        url: String!
-        author: String!
-        mergedAt: Date!
-    }
+	type PullRequest {
+		url: String!
+		author: String!
+		mergedAt: Date!
+	}
 
-    scalar Date
+	scalar Date
 
-    input PullRequestInput {
-        url: String!
-        author: String!
-        mergedAt: Date!
-    }
+	input PullRequestInput {
+		url: String!
+		author: String!
+		mergedAt: Date!
+	}
 
-    type Bounty {
-        tvl: Float!
-        contractAddress: String!
-        id: ID!
-        claimantPullRequest: PullRequest
-        watchingUserIds: [String]
-        watchingUsers(
-            after: ID
-            limit: Int!
-            orderBy: String
-            sortOrder: String
-        ): UserConnection!
-    }
-    type User {
-        id: ID
-        userAddress: String!
-        watchedBountyIds: [String]
-        watchedBounties(
-            after: ID
-            limit: Int!
-            orderBy: String
-            sortOrder: String
-        ): BountyConnection!
-    }
+	type Bounty {
+		tvl: Float!
+		contractAddress: String!
+		id: ID!
+		claimantPullRequest: PullRequest
+		watchingUserIds: [String]
+		watchingUsers(
+			after: ID
+			limit: Int!
+			orderBy: String
+			sortOrder: String
+		): UserConnection!
+	}
+	type User {
+		id: ID
+		userAddress: String!
+		watchedBountyIds: [String]
+		watchedBounties(
+			after: ID
+			limit: Int!
+			orderBy: String
+			sortOrder: String
+		): BountyConnection!
+	}
 
-    type UserConnection {
-        users: [User]
-        cursor: ID
-    }
+	type UserConnection {
+		users: [User]
+		cursor: ID
+	}
 
-    type BountyConnection {
-        bounties: [Bounty]
-        cursor: ID
-    }
+	type BountyConnection {
+		bounties: [Bounty]
+		cursor: ID
+	}
 
-    type BatchPayload {
-        count: Long!
-    }
-    scalar Long
+	type BatchPayload {
+		count: Long!
+	}
+	scalar Long
 
-    type Query {
-        bountiesConnection(
-            after: ID
-            limit: Int!
-            orderBy: String
-            sortOrder: String
-        ): BountyConnection
-        usersConnection(
-            after: ID
-            limit: Int
-            orderBy: String
-            sortOrder: String
-        ): UserConnection
-        bounty(contractAddress: String!): Bounty
-        user(userAddress: String!): User
-    }
-    type Mutation {
-        createBounty(tvl: Float!, contractAddress: String!): Bounty!
-        updateBounty(tvl: Float!, contractAddress: String!): BatchPayload!
-        watchBounty(userAddress: String, contractAddress: String): User
-        unWatchBounty(userAddress: String, contractAddress: String): User
-    }
+	type Query {
+		bountiesConnection(
+			after: ID
+			limit: Int!
+			orderBy: String
+			sortOrder: String
+		): BountyConnection
+		usersConnection(
+			after: ID
+			limit: Int
+			orderBy: String
+			sortOrder: String
+		): UserConnection
+		bounty(contractAddress: String!): Bounty
+		user(userAddress: String!): User
+	}
+	type Mutation {
+		createBounty(tvl: Float!, contractAddress: String!): Bounty!
+		updateBounty(tvl: Float!, contractAddress: String!): BatchPayload!
+		watchBounty(userAddress: String, contractAddress: String): User
+		unWatchBounty(userAddress: String, contractAddress: String): User
+	}
 `;
 
 module.exports = typeDefs;
