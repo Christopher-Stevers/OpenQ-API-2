73fad37fd3acdfe37a1318c5e6147945e3cd3736
Christopher Stevers <christopher.stevers@protonmail.com>
2022-06-10 14:00:42 -0400

    prettier/eslint

--- src/indexer.js
+++ src/indexer.js
@@ -1,94 +1,99 @@
-const axios = require('axios')
-const fetch = require('cross-fetch')
-const { ethers } = require('ethers')
-const { ApolloClient, InMemoryCache, gql, HttpLink } = require('@apollo/client')
-const { GET_BOUNTY_DEPOSITS_DATA, CREATE_BOUNTY } = require('./query/query')
-const tokenMetadata = require('./local.json')
+const axios = require('axios');
+const fetch = require('cross-fetch');
+const { ethers } = require('ethers');
+const {
+	ApolloClient,
+	InMemoryCache,
+	gql,
+	HttpLink,
+} = require('@apollo/client');
+const { GET_BOUNTY_DEPOSITS_DATA, CREATE_BOUNTY } = require('./query/query');
+const tokenMetadata = require('./local.json');
 
 const subGraphClient = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-        fetch,
-        uri: 'http://graph_node:8000/subgraphs/name/openqdev/openq',
-    }),
-})
+	cache: new InMemoryCache(),
+	link: new HttpLink({
+		fetch,
+		uri: 'http://graph_node:8000/subgraphs/name/openqdev/openq',
+	}),
+});
 
 const tvlClient = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-        fetch,
-        uri: 'http://openq-api:8080/',
-    }),
-})
+	cache: new InMemoryCache(),
+	link: new HttpLink({
+		fetch,
+		uri: 'http://openq-api:8080/',
+	}),
+});
 
 const fetchBounties = async () => {
-    async function getTokenValues(tokenBalances) {
-        const tokenVolumes = {}
-        tokenBalances.forEach((tokenBalance) => {
-            const tokenAddress =
-                tokenMetadata[
-                    ethers.utils.getAddress(tokenBalance.tokenAddress)
-                ].address
-            tokenVolumes[tokenAddress] = tokenBalance.volume
-        })
+	async function getTokenValues(tokenBalances) {
+		const tokenVolumes = {};
+		tokenBalances.forEach((tokenBalance) => {
+			const tokenAddress =
+				tokenMetadata[
+					ethers.utils.getAddress(tokenBalance.tokenAddress)
+				].address;
+			tokenVolumes[tokenAddress] = tokenBalance.volume;
+		});
 
-        const params = { tokenVolumes, network: 'polygon-pos' }
-        const url = 'http://openq-coinapi:8081/tvl'
-        // only query tvl for bounties that have deposits
-        if (JSON.stringify(params.tokenVolumes) !== '{}') {
-            try {
-                const { data } = await axios.post(url, params)
-                return data
-            } catch (error) {
-                // continue regardless
-            }
-            return []
-        }
-        return []
-    }
-    const depositResponse = await subGraphClient.query({
-        query: gql(GET_BOUNTY_DEPOSITS_DATA),
-    })
-    const { deposits } = depositResponse.data
+		const params = { tokenVolumes, network: 'polygon-pos' };
+		const url = 'http://openq-coinapi:8081/tvl';
+		// only query tvl for bounties that have deposits
+		if (JSON.stringify(params.tokenVolumes) !== '{}') {
+			try {
+				const { data } = await axios.post(url, params);
+				return data;
+			} catch (error) {
+				// continue regardless
+			}
+			return [];
+		}
+		return [];
+	}
+	const depositResponse = await subGraphClient.query({
+		query: gql(GET_BOUNTY_DEPOSITS_DATA),
+	});
+	const { deposits } = depositResponse.data;
 
-    const tokenValues = await getTokenValues(deposits)
-    const TVLS = deposits.map((tokenBalance) => {
-        const tokenAddress = ethers.utils.getAddress(tokenBalance.tokenAddress)
-        const tokenValueAddress = tokenMetadata[tokenAddress].address
-        const { volume } = tokenBalance
+	const tokenValues = await getTokenValues(deposits);
+	const TVLS = deposits.map((tokenBalance) => {
+		const tokenAddress = ethers.utils.getAddress(tokenBalance.tokenAddress);
+		const tokenValueAddress = tokenMetadata[tokenAddress].address;
+		const { volume } = tokenBalance;
 
-        const bigNumberVolume = ethers.BigNumber.from(volume.toString())
-        const decimals = parseInt(tokenMetadata[tokenAddress].decimals, 10)
+		const bigNumberVolume = ethers.BigNumber.from(volume.toString());
+		const decimals = parseInt(tokenMetadata[tokenAddress].decimals, 10);
 
-        const formattedVolume = ethers.utils.formatUnits(
-            bigNumberVolume,
-            decimals
-        )
-        const totalValue =
-            formattedVolume *
-            tokenValues.tokenPrices[tokenValueAddress.toLowerCase()]
+		const formattedVolume = ethers.utils.formatUnits(
+			bigNumberVolume,
+			decimals
+		);
+		const totalValue =
+			formattedVolume *
+			tokenValues.tokenPrices[tokenValueAddress.toLowerCase()];
 
-        return { id: tokenBalance.bounty.id, totalValue }
-    })
-    return TVLS
-}
+		return { id: tokenBalance.bounty.id, totalValue };
+	});
+	return TVLS;
+};
 const updateTvls = async (values) => {
-    const pending = []
-    for (let i = 0; i < values.length; i += 1) {
-        const value = values[i]
-        const contractAddress = value.id
-        const tvl = parseFloat(value.totalValue)
-        const result = tvlClient.mutate({
-            mutation: gql(CREATE_BOUNTY),
-            variables: { contractAddress, tvl },
-        })
-        pending.push(result)
-    }
-    await Promise.all(pending)
-}
+	const pending = [];
+	for (let i = 0; i < values.length; i += 1) {
+		const value = values[i];
+		const contractAddress = value.id;
+		const tvl = parseFloat(value.totalValue);
+		const result = tvlClient.mutate({
+			mutation: gql(CREATE_BOUNTY),
+			variables: { contractAddress, tvl },
+		});
+		pending.push(result);
+	}
+	await Promise.all(pending);
+};
 const indexer = async () => {
-    const TVLS = await fetchBounties()
-    await updateTvls(TVLS)
-}
+	const TVLS = await fetchBounties();
+	await updateTvls(TVLS);
+};
 
-module.exports = indexer
+module.exports = indexer;
