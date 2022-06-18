3f82c21a46c9478f26d36dfd32397d422179f6ce
FlacoJones <andrew@openq.dev>
2022-06-18 15:10:36 -0500

    moves bountiesConnection to /bounty/query

--- src/resolvers/bounty/query.js
+++ src/resolvers/bounty/query.js
@@ -3,6 +3,24 @@ const Query = {
 		prisma.bounty.findUnique({
 			where: { address: args.address },
 		}),
+	bountiesConnection: async (parent, args, { req, prisma }) => {
+		const cursor = args.after ? { address: args.after } : undefined;
+		const bounties = await prisma.bounty.findMany({
+			skip: args.after ? 1 : 0,
+			cursor,
+			where: { organizationId: args.organizationId },
+			take: args.limit,
+			orderBy: [
+				{ [args.orderBy]: args.sortOrder },
+				{ [args.orderBy && 'address']: args.orderBy && 'asc' },
+			],
+			include: { watchingUsers: true },
+		});
+		return {
+			bounties,
+			cursor: bounties[bounties.length - 1].address,
+		};
+	},
 };
 
 module.exports = Query;
