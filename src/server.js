3596c9a6102330a321a3f44ace116ba466f23bd8
FlacoJones <andrew@openq.dev>
2022-06-22 13:01:38 -0500

    adds introspection

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ const server = new ApolloServer({
 	resolvers,
 	context: createContext,
 	plugins: [apolloLogger, ApolloServerPluginLandingPageGraphQLPlayground],
+	introspection: true,
 	cors: true
 });
 
