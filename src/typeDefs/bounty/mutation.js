5a7985f7adb1dbcb89476299205df463ecf492e9
FlacoJones <andrew@openq.dev>
2022-06-24 14:57:01 -0500

    formatting update

--- src/typeDefs/bounty/mutation.js
+++ src/typeDefs/bounty/mutation.js
@@ -3,11 +3,7 @@ const { gql } = require('apollo-server');
 const mutationDefs = gql`
   extend type Mutation {
 		createBounty(address: String!, bountyId: String!, organizationId: String!): Bounty!
-		updateBounty(
-			address: String!
-			tvl: Float!
-			organizationId: String
-		): Bounty!
+		updateBounty(address: String!, tvl: Float!, organizationId: String): Bounty!
 		watchBounty(userAddress: String, contractAddress: String): User
 		unWatchBounty(userAddress: String, contractAddress: String): User
 		addToTvl(address: String, tokenBalance: JSON, add: Boolean): Bounty
