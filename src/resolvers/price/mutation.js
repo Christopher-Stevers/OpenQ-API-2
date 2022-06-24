8ce3f1a1a24663f10c932eaf9f0238c79a6d996d
FlacoJones <andrew@openq.dev>
2022-06-24 14:54:09 -0500

    adds authentication for token price updates

--- src/resolvers/price/mutation.js
+++ src/resolvers/price/mutation.js
@@ -1,19 +1,26 @@
+const { AuthenticationError } = require('apollo-server');
+
 const Mutation = {
-	updatePrices: async (parent, args, { req, prisma }) => prisma.prices.upsert({
-		where: {
-			pricesId: args.pricesId,
-		},
-		update: {
-			pricesId: args.pricesId,
-			timestamp: Date.now(),
-			priceObj: args.priceObj
-		},
-		create: {
-			pricesId: args.pricesId,
-			timestamp: Date.now(),
-			priceObj: args.priceObj
+	updatePrices: async (parent, args, { req, prisma }) => {
+		if (req.headers.authorization !== process.env.OPENQ_API_SECRET) {
+			throw new AuthenticationError();
 		}
-	})
+		return prisma.prices.upsert({
+			where: {
+				pricesId: args.pricesId,
+			},
+			update: {
+				pricesId: args.pricesId,
+				timestamp: Date.now(),
+				priceObj: args.priceObj
+			},
+			create: {
+				pricesId: args.pricesId,
+				timestamp: Date.now(),
+				priceObj: args.priceObj
+			}
+		});
+	}
 };
 
 module.exports = Mutation;
