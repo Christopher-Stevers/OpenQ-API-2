ce68a2f5a6753b5463bcb997d886d3e48ec68e08
Christopher Stevers <christopher.stevers@protonmail.com>
2022-07-01 06:41:57 -0400

    updated tests

--- src/__tests__/bounty/watchBounty.test.js
+++ src/__tests__/bounty/watchBounty.test.js
@@ -30,12 +30,12 @@ describe('Watch and unwatch respond properly to user.', () => {
 		const client = getAuthenticatedClient('_', input.signature);
 		const watchData = await client.mutate({
 			mutation: WATCH_BOUNTY,
-			variables: { userAddress, contractAddress, signature: input.signature }
+			variables: { userAddress, contractAddress }
 		});
 		expect(watchData.data.watchBounty.watchingUserIds).toContain(input.signer);
 		const unWatchData = await client.mutate({
 			mutation: UNWATCH_BOUNTY,
-			variables: { userAddress: input.signer, contractAddress, signature: input.signature }
+			variables: { userAddress: input.signer, contractAddress }
 		});
 		const userData = await client.query({
 			query: GET_USER_BY_HASH,
@@ -46,22 +46,24 @@ describe('Watch and unwatch respond properly to user.', () => {
 
 	});
 
-	it('Should return bounty, but not watch when unsigned user attempts.', async () => {
+	it('Should throw when unsigned user attempts to watch.', async () => {
 		const input = {
 			signature: '0xdf448d548305c70c8d5fb08d1c5a0cb3adfa6668eafa7a154b2c1eecf10c80ea1cc3afe23cc89f74323b02db7322b78c17add446c70ccbbb7a6ecfe58ac8b4bd1b',
 			signer: '0xa7b7DcBb35A58294Ba9E51cC9AA20670E124536b',
 			value: 'OpenQ'
 		};
 		const client = getAuthenticatedClient('_', input.signature);
-		const watchData = await client.mutate({
-			mutation: WATCH_BOUNTY,
-			variables: { userAddress: input.signer, contractAddress, signature: input.signature }
-		});
-		expect(watchData.data.watchBounty).toBe(null);
+		const watch = async () => {
+			return await client.mutate({
+				mutation: WATCH_BOUNTY,
+				variables: { userAddress: input.signer, contractAddress, signature: input.signature }
+			});
+		};
+		await expect(watch).rejects.toThrow(Error);
 
 	});
 
-	it('Should return bounty, but not unWatch when unsigned user attempts.', async () => {
+	it('Should throw when unsigned user attempts to unwatch.', async () => {
 		const input = {
 			signature: '0xdf448d548305c70c8d5fb08d1c5a0cb3adfa6668eafa7a154b2c1eecf10c80ea1cc3afe23cc89f74323b02db7322b78c17add446c70ccbbb7a6ecfe58ac8b4bd1b',
 			signer: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
@@ -71,15 +73,17 @@ describe('Watch and unwatch respond properly to user.', () => {
 		const client = getAuthenticatedClient('_', input.signature);
 		const watchData = await client.mutate({
 			mutation: WATCH_BOUNTY,
-			variables: { userAddress: input.signer, contractAddress, signature: input.signature }
+			variables: { userAddress: input.signer, contractAddress }
 		});
 		expect(watchData.data.watchBounty.watchingUserIds).toContain(input.signer);
+		const unWatch = async () => {
+			return await client.mutate({
+				mutation: UNWATCH_BOUNTY,
+				variables: { userAddress: input.unsigned, contractAddress }
+			});
 
-		const unWatchData = await client.mutate({
-			mutation: UNWATCH_BOUNTY,
-			variables: { userAddress: input.unsigned, contractAddress, signature: input.signature }
-		});
-		expect(unWatchData.data.unWatchBounty).toBe(null);
+		};
+		await expect(unWatch()).rejects.toThrow(Error);
 
 	});
 
