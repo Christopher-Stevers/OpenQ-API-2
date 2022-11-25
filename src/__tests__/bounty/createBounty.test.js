e6124fc06db08faf37d10e0662d55b4d584ea90e
FlacoJones <andrew@openq.dev>
2022-11-25 15:46:25 -0600

    refactors afterEach to only tests that make bounties successfully

--- src/__tests__/bounty/createBounty.test.js
+++ src/__tests__/bounty/createBounty.test.js
@@ -11,43 +11,51 @@ dotenv.config({ path: '../../../.env.test' });
 
 describe('createBounty', () => {
 	const contractAddress = '0x8daf17assdfdf20c9dba35f005b6324f493785d239719d';
+	const organizationId = 'organizationId';
+
 	const authenticatedClient = getAuthenticatedClient('secret123!', 'signature');
 	const unauthenticatedClient = getAuthenticatedClient('incorrect_secret', 'signature');
 
-	it('Authenticated client can create bounty', async () => {
-		await authenticatedClient.mutate({
-			mutation: CREATE_NEW_BOUNTY,
-			variables: { address: contractAddress, organizationId: 'mdp', bountyId: 'sdf', repositoryId: 'repoId', type: '1' }
+	describe('Successful', () => {
+		afterEach(async () => {
+			await clearDb();
 		});
 
-		const { data } = await authenticatedClient.query({
-			query: GET_BOUNTY_BY_ID,
-			variables: { contractAddress }
+		it('Authenticated client can create bounty', async () => {
+			await authenticatedClient.mutate({
+				mutation: CREATE_NEW_BOUNTY,
+				variables: { address: contractAddress, organizationId, bountyId: 'sdf', repositoryId: 'repoId', type: '1' }
+			});
+	
+			const { data } = await authenticatedClient.query({
+				query: GET_BOUNTY_BY_ID,
+				variables: { contractAddress }
+			});
+	
+			expect(data.bounty).toMatchObject({
+				'tvl': 0,
+				'bountyId': 'sdf',
+				'type': '1',
+				'organization': {
+					'id': organizationId
+				},
+				'repository': {
+					'id': 'repoId'
+				}
+			});
 		});
+	});
 
-		expect(data.bounty).toMatchObject({
-			'tvl': 0,
-			'bountyId': 'sdf',
-			'type': '1',
-			'organization': {
-				'id': 'mdp'
-			},
-			'repository': {
-				'id': 'repoId'
+	describe('Unsuccessful', () => {
+		it('should fail for unauthenticated calls', async () => {
+			try {
+				await unauthenticatedClient.mutate({
+					mutation: CREATE_NEW_BOUNTY,
+					variables: { address: contractAddress, organizationId: 'mdp', bountyId: 'sdf', repositoryId: 'repoId', type: '1' }
+				});
+			} catch (error) {
+				expect(error.graphQLErrors[0].extensions.code).toEqual('UNAUTHENTICATED');
 			}
 		});
-
-		await clearDb();
-	});
-
-	it('should fail for unauthenticated calls', async () => {
-		try {
-			await unauthenticatedClient.mutate({
-				mutation: CREATE_NEW_BOUNTY,
-				variables: { address: contractAddress, organizationId: 'mdp', bountyId: 'sdf', repositoryId: 'repoId', type: '1' }
-			});
-		} catch (error) {
-			expect(error.graphQLErrors[0].extensions.code).toEqual('UNAUTHENTICATED');
-		}
-	});
+	 });
 });
