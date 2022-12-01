2b6abbe4615ce1808120ba0410626d8c674c51de
FlacoJones <andrew@openq.dev>
2022-12-01 14:25:03 -0600

    userAuth tests

--- src/__tests__/auth/userAuth.test.js
+++ src/__tests__/auth/userAuth.test.js
@@ -11,21 +11,39 @@ describe('userAuth', () => {
 	const userId = '123';
 	const github = 'github';
 	const email = 'email';
-	const args = { email, github, userId };
+	const args_GITHUB = { github, userId };
+	const args_EMAIL = { email, userId };
+	const args_BOTH = { email, github, userId };
 	const invalidArgs = { userId };
 
 	const prisma = {};
 	
 	const req = null;
 
-	it('should throw Error if no email or github passed', async () => {
+	it('should return error if no email or github passed', async () => {
 		const result = await checkUserAuth(prisma, req, invalidArgs, MockEmailClient, MockGithubClient);
 		expect(result).toMatchObject({ error: true, errorMessage: 'Must provide a an email OR github' });
 	});
 
+	it('should return error if invalid auth - EMAIL', async () => {
+		MockEmailClient.isValidEmail = false;
+		const result = await checkUserAuth(prisma, req, args_EMAIL, MockEmailClient, MockGithubClient);
+		expect(result).toMatchObject({ error: true, errorMessage: 'Email not authorized' });
+	});
+
+	it('should return error if invalid auth - GITHUB', async () => {
+		MockGithubClient.isValidGithub = false;
+		const result = await checkUserAuth(prisma, req, args_GITHUB, MockEmailClient, MockGithubClient);
+		expect(result).toMatchObject({ error: true, errorMessage: 'Github not authorized' });
+	});
+
 	it('should return error false and identifier if successful - GITHUB', async () => {
-		const result = await checkUserAuth(prisma, req, args, MockEmailClient, MockGithubClient);
-		console.log(result);
+		const result = await checkUserAuth(prisma, req, args_GITHUB, MockEmailClient, MockGithubClient);
 		expect(result).toMatchObject({ error: false, errorMessage: null, github: 'github' });
+	});
+
+	it('should return error false and identifier if successful - EMAIL', async () => {
+		const result = await checkUserAuth(prisma, req, args_EMAIL, MockEmailClient, MockGithubClient);
+		expect(result).toMatchObject({ error: false, errorMessage: null, email: 'email' });
 	});
 }); 
