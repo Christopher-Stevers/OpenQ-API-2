6aec69f3aea66dd44507d6178af4508bc4133a8a
FlacoJones <andrew@openq.dev>
2022-12-01 14:06:02 -0600

    updates

--- src/__tests__/auth/userAuth.test.js
+++ src/__tests__/auth/userAuth.test.js
@@ -1,7 +1,22 @@
-const userAuth = require('../../resolvers/utils/userAuth');
+const checkUserAuth = require('../../resolvers/utils/userAuth');
+const MockEmailClient = require('../../utils/auth/email/MockEmailClient');
+const MockGithubClient = require('../../utils/auth/github/MockGithubClient');
 
-describe('userAuth', () => { 
-	it('should return a function', () => {
-		expect(typeof userAuth).toBe('function');
+describe('userAuth', () => {
+	const userId = '123';
+	const github = 'github';
+	const email = 'email';
+	const args = { email, github, userId };
+
+	const prisma = {};
+	
+	const req = {};
+
+	it('should return a function', async () => {
+		MockGithubClient.isValidGithub = true;
+		MockEmailClient.isValidEmail = true;
+		const result = await checkUserAuth(prisma, req, args, MockEmailClient, MockGithubClient);
+		console.log(result);
+		expect(result).toMatchObject({ github: 'github' });
 	});
-});
+}); 
