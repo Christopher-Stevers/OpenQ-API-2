95bb76d4757686e1e3c0cfd20bef6123afba43eb
FlacoJones <andrew@openq.dev>
2022-12-03 11:51:50 -0600

    adds a VIEWER_CAN_ADMINISTER_REPOSITORY

---
+++ src/utils/auth/github/verifyUserCanAdministerRepository.js
@@ -0,0 +1,65 @@
+// Third Party
+const axios = require('axios');
+
+// Issue Query
+const VIEWER_CAN_ADMINISTER_REPOSITORY = require('./query/VIEWER_CAN_ADMINISTER_REPOSITORY');
+
+// Errors
+const {
+	GITHUB_OAUTH_TOKEN_LACKS_PRIVILEGES,
+	UNKNOWN_ERROR,
+	RATE_LIMITED,
+	INVALID_GITHUB_OAUTH_TOKEN,
+	NO_GITHUB_OAUTH_TOKEN
+} = require('./errors/errors');
+
+/***
+ *  Verifies the OAuth token holder matches 
+ * ***/
+const verifyUserCanAdministerRepository = async (req, userId) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const signatureRegex = /github_oauth_token_unsigned=\w+/;
+			const regexMatch = req.headers.cookie.match(signatureRegex);
+			
+			let token;
+			if (regexMatch === null) {
+				return reject(NO_GITHUB_OAUTH_TOKEN({ userId }));
+			} else {
+				token = req.headers.cookie.match(signatureRegex)[0].slice(28);
+			}
+			
+			const resultViewer = await axios
+				.post(
+					'https://api.github.com/graphql',
+					{
+						query: VIEWER_CAN_ADMINISTER_REPOSITORY
+					},
+					{
+						headers: {
+							'Authorization': 'token ' + token,
+						},
+					}
+				);
+
+			if (resultViewer.data.errors && resultViewer.data.errors[0].type == 'RATE_LIMITED') {
+				return reject(RATE_LIMITED({ userId }));
+			}
+
+			const verifyUserCanAdministerRepository = resultViewer.data.node.viewerCanAdminister;
+
+			if (verifyUserCanAdministerRepository) {
+				return resolve(true);
+			} else {
+				return reject(INVALID_GITHUB_OAUTH_TOKEN({userId}));
+			}
+		} catch (error) {
+			if (error.response && error.response.status == 401) {
+				return reject(GITHUB_OAUTH_TOKEN_LACKS_PRIVILEGES({ userId }));
+			}
+			return reject(UNKNOWN_ERROR({ userId, error }));
+		}
+	});
+};
+
+module.exports = verifyUserCanAdministerRepository;
