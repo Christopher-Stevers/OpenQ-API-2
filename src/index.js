ad339f7d02881cd8cce90bef479ecca4e57f3b37
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-02-09 15:12:47 +0000

    limited request size

--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
 const server = require('./server');
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 async function startApolloServer() {
 	const app = express();
+	const corsOptions = {
+		origin: [...process.env.ORIGIN.split(',')],
+		credentials: true,
+	};
 
 	await server.start();
+	server.applyMiddleware({ app, cors: corsOptions, path: '/' });
+	// Middleware function to limit the size of incoming requests
 
-	server.applyMiddleware({ app });
+	const MAX_REQUEST_SIZE = 1024 * 1024;
+	const limitRequestSize = (req, res, next) => {
+		const payloadSize = Buffer.byteLength(JSON.stringify(req.body));
+		if (payloadSize > MAX_REQUEST_SIZE) {
+			res.status(413).send('Payload too large');
+		} else {
+			next();
+		}
+	};
 
-	app.use((req, res) => {
-		res.status(200);
+	// Apply the middleware function to all incoming requests
+	app.use(limitRequestSize);
+	app.use(cors(corsOptions));
 
-		res.send('Hello!');
-		res.end();
-	});
-
-	await new Promise((resolve) => app.listen({ port: 4000 }, resolve));
-
-	console.log(
-		`🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+	app.listen({ port: 4000 }, () =>
+		console.log(
+			`🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+		)
 	);
-
-	return { server, app };
 }
 startApolloServer();
